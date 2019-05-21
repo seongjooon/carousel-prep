97ead5ca396e362ae8269ea1cb316bbe2363c4f3
MrKamake <seongjooon@gmail.com>
2019-05-21 21:04:00 +0900

    create div box

--- app/index.js
+++ app/index.js
@@ -4,5 +4,3 @@ import 'styles/index.css';
 // ================================
 // START YOUR APP HERE
 // ================================
-
-alert('Getting Started? Look for index.js inside app directory.');
