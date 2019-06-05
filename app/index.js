af42e394f033a80af4b9edbd7b5026e3661fc49f
MrKamake <seongjooon@gmail.com>
2019-06-05 13:23:57 +0900

    modified

--- app/index.js
+++ app/index.js
@@ -1,27 +1,25 @@
 // Load application styles
 import 'styles/index.css';
 
-const el = el => document.getElementById(el);
-
-function Carusel(imageNum) {
+function Carusel(numOfImages) {
     this.focus = 0;
-    this.imageNum = imageNum;
+    this.numOfImages = numOfImages;
 }
 
 Carusel.prototype.moveHorizontal = function (direction) {
     direction === "left" ? this.focus++ : this.focus--;
-    if (this.focus === 1) this.focus = -this.imageNum + 1;
-    if (this.focus === -this.imageNum) this.focus = 0;
-    el("image-list").style.transform = 'translate(' + 600 * this.focus + 'px)';
-}
+    if (this.focus === 1) this.focus = -this.numOfImages + 1;
+    if (this.focus === -this.numOfImages) this.focus = 0;
+    document.getElementById("image-list").style.transform = 'translate(' + 600 * this.focus + 'px)';
+};
 
-Carusel.prototype.getBtn = function (direction) {
-    el(direction + 'Btn')
-        .addEventListener('click', Carusel.prototype.moveHorizontal.bind(this, direction));
-}
+Carusel.prototype.bindButtonAction = function (direction) {
+    document.getElementById(direction + 'Btn')
+        .addEventListener('click', this.moveHorizontal.bind(this, direction));
+};
 
-const imageNum = el("image-list").children.length;
-const carusel = new Carusel(imageNum);
+const numOfImages = document.getElementById("image-list").children.length;
+const carusel = new Carusel(numOfImages);
 
-carusel.getBtn('left');
-carusel.getBtn('right');
+carusel.bindButtonAction('left');
+carusel.bindButtonAction('right');
